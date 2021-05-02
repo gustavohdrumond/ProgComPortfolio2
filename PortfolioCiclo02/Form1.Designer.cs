fb6c070905a637c87499b7e3a4af6826339518aa
Gustavo Henrique Drumond Leão <35820435+gustavohdrumond@users.noreply.github.com>
2021-05-02 20:14:50 -0300

    Versão entregue do Projeto

---
+++ PortfolioCiclo02/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace PortfolioCiclo02
+{
+    partial class PortfolioCiclo03
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instrucaoTextoA = new System.Windows.Forms.Label();
+            this.instrucaoTextoB = new System.Windows.Forms.Label();
+            this.instrucaoTextoC = new System.Windows.Forms.Label();
+            this.finalDelta = new System.Windows.Forms.Label();
+            this.finalX1 = new System.Windows.Forms.Label();
+            this.finalX2 = new System.Windows.Forms.Label();
+            this.resultadoDelta = new System.Windows.Forms.Label();
+            this.resultadoX1 = new System.Windows.Forms.Label();
+            this.resultadoX2 = new System.Windows.Forms.Label();
+            this.titulo = new System.Windows.Forms.Label();
+            this.botaoCalculo = new System.Windows.Forms.Button();
+            this.valorEntradaA = new System.Windows.Forms.TextBox();
+            this.valorEntradaB = new System.Windows.Forms.TextBox();
+            this.valorEntradaC = new System.Windows.Forms.TextBox();
+            this.titulo2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // instrucaoTextoA
+            // 
+            this.instrucaoTextoA.AutoSize = true;
+            this.instrucaoTextoA.Location = new System.Drawing.Point(56, 82);
+            this.instrucaoTextoA.Name = "instrucaoTextoA";
+            this.instrucaoTextoA.Size = new System.Drawing.Size(63, 15);
+            this.instrucaoTextoA.TabIndex = 0;
+            this.instrucaoTextoA.Text = "Valor de A:";
+            // 
+            // instrucaoTextoB
+            // 
+            this.instrucaoTextoB.AutoSize = true;
+            this.instrucaoTextoB.Location = new System.Drawing.Point(56, 111);
+            this.instrucaoTextoB.Name = "instrucaoTextoB";
+            this.instrucaoTextoB.Size = new System.Drawing.Size(62, 15);
+            this.instrucaoTextoB.TabIndex = 1;
+            this.instrucaoTextoB.Text = "Valor de B:";
+            // 
+            // instrucaoTextoC
+            // 
+            this.instrucaoTextoC.AutoSize = true;
+            this.instrucaoTextoC.Location = new System.Drawing.Point(56, 140);
+            this.instrucaoTextoC.Name = "instrucaoTextoC";
+            this.instrucaoTextoC.Size = new System.Drawing.Size(63, 15);
+            this.instrucaoTextoC.TabIndex = 2;
+            this.instrucaoTextoC.Text = "Valor de C:";
+            // 
+            // finalDelta
+            // 
+            this.finalDelta.AutoSize = true;
+            this.finalDelta.Location = new System.Drawing.Point(522, 108);
+            this.finalDelta.Name = "finalDelta";
+            this.finalDelta.Size = new System.Drawing.Size(37, 15);
+            this.finalDelta.TabIndex = 6;
+            this.finalDelta.Text = "Delta:";
+            // 
+            // finalX1
+            // 
+            this.finalX1.AutoSize = true;
+            this.finalX1.Location = new System.Drawing.Point(522, 160);
+            this.finalX1.Name = "finalX1";
+            this.finalX1.Size = new System.Drawing.Size(25, 15);
+            this.finalX1.TabIndex = 7;
+            this.finalX1.Text = "x1: ";
+            // 
+            // finalX2
+            // 
+            this.finalX2.AutoSize = true;
+            this.finalX2.Location = new System.Drawing.Point(522, 222);
+            this.finalX2.Name = "finalX2";
+            this.finalX2.Size = new System.Drawing.Size(25, 15);
+            this.finalX2.TabIndex = 8;
+            this.finalX2.Text = "x2: ";
+            // 
+            // resultadoDelta
+            // 
+            this.resultadoDelta.AutoSize = true;
+            this.resultadoDelta.Location = new System.Drawing.Point(593, 108);
+            this.resultadoDelta.Name = "resultadoDelta";
+            this.resultadoDelta.Size = new System.Drawing.Size(0, 15);
+            this.resultadoDelta.TabIndex = 9;
+            // 
+            // resultadoX1
+            // 
+            this.resultadoX1.AutoSize = true;
+            this.resultadoX1.Location = new System.Drawing.Point(593, 160);
+            this.resultadoX1.Name = "resultadoX1";
+            this.resultadoX1.Size = new System.Drawing.Size(0, 15);
+            this.resultadoX1.TabIndex = 10;
+            // 
+            // resultadoX2
+            // 
+            this.resultadoX2.AutoSize = true;
+            this.resultadoX2.Location = new System.Drawing.Point(593, 222);
+            this.resultadoX2.Name = "resultadoX2";
+            this.resultadoX2.Size = new System.Drawing.Size(0, 15);
+            this.resultadoX2.TabIndex = 11;
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titulo.Location = new System.Drawing.Point(522, 24);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(137, 31);
+            this.titulo.TabIndex = 12;
+            this.titulo.Text = "Resultado";
+            // 
+            // botaoCalculo
+            // 
+            this.botaoCalculo.Location = new System.Drawing.Point(56, 178);
+            this.botaoCalculo.Name = "botaoCalculo";
+            this.botaoCalculo.Size = new System.Drawing.Size(258, 23);
+            this.botaoCalculo.TabIndex = 13;
+            this.botaoCalculo.Text = "Calcular";
+            this.botaoCalculo.UseVisualStyleBackColor = true;
+            this.botaoCalculo.Click += new System.EventHandler(this.botaoCalculo_Click);
+            // 
+            // valorEntradaA
+            // 
+            this.valorEntradaA.Location = new System.Drawing.Point(128, 79);
+            this.valorEntradaA.Name = "valorEntradaA";
+            this.valorEntradaA.Size = new System.Drawing.Size(186, 23);
+            this.valorEntradaA.TabIndex = 14;
+            // 
+            // valorEntradaB
+            // 
+            this.valorEntradaB.Location = new System.Drawing.Point(128, 108);
+            this.valorEntradaB.Name = "valorEntradaB";
+            this.valorEntradaB.Size = new System.Drawing.Size(186, 23);
+            this.valorEntradaB.TabIndex = 15;
+            // 
+            // valorEntradaC
+            // 
+            this.valorEntradaC.Location = new System.Drawing.Point(128, 137);
+            this.valorEntradaC.Name = "valorEntradaC";
+            this.valorEntradaC.Size = new System.Drawing.Size(186, 23);
+            this.valorEntradaC.TabIndex = 16;
+            // 
+            // titulo2
+            // 
+            this.titulo2.AutoSize = true;
+            this.titulo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titulo2.Location = new System.Drawing.Point(21, 24);
+            this.titulo2.Name = "titulo2";
+            this.titulo2.Size = new System.Drawing.Size(425, 31);
+            this.titulo2.TabIndex = 17;
+            this.titulo2.Text = "Calculadora - Equação de 2º Grau";
+            // 
+            // PortfolioCiclo03
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(867, 291);
+            this.Controls.Add(this.titulo2);
+            this.Controls.Add(this.valorEntradaC);
+            this.Controls.Add(this.valorEntradaB);
+            this.Controls.Add(this.valorEntradaA);
+            this.Controls.Add(this.botaoCalculo);
+            this.Controls.Add(this.titulo);
+            this.Controls.Add(this.resultadoX2);
+            this.Controls.Add(this.resultadoX1);
+            this.Controls.Add(this.resultadoDelta);
+            this.Controls.Add(this.finalX2);
+            this.Controls.Add(this.finalX1);
+            this.Controls.Add(this.finalDelta);
+            this.Controls.Add(this.instrucaoTextoC);
+            this.Controls.Add(this.instrucaoTextoB);
+            this.Controls.Add(this.instrucaoTextoA);
+            this.Name = "PortfolioCiclo03";
+            this.Text = "Programação Comercial - Portfólio Ciclo 03";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instrucaoTextoA;
+        private System.Windows.Forms.Label instrucaoTextoB;
+        private System.Windows.Forms.Label instrucaoTextoC;
+        private System.Windows.Forms.Label finalDelta;
+        private System.Windows.Forms.Label finalX1;
+        private System.Windows.Forms.Label finalX2;
+        private System.Windows.Forms.Label resultadoDelta;
+        private System.Windows.Forms.Label resultadoX1;
+        private System.Windows.Forms.Label resultadoX2;
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.Label titulo2;
+        private System.Windows.Forms.Button botaoCalculo;
+        private System.Windows.Forms.TextBox valorEntradaA;
+        private System.Windows.Forms.TextBox valorEntradaB;
+        private System.Windows.Forms.TextBox valorEntradaC;
+    }
+}
---
+++ PortfolioCiclo02/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PortfolioCiclo02
+{
+    public partial class PortfolioCiclo03 : Form
+    {
+        public PortfolioCiclo03()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botaoCalculo_Click(object sender, EventArgs e)
+        {
+            double a = 0, b = 0, c = 0;
+
+            a = Convert.ToDouble(valorEntradaA.Text);
+            b = Convert.ToDouble(valorEntradaB.Text);
+            c = Convert.ToDouble(valorEntradaC.Text);
+
+            delta(a, b, c);
+            x1(a, b, c);
+            x2(a, b, c);
+        }
+
+        private void delta(double a, double b, double c)
+        {
+            double delta = (b * b) - (4 * (a * c));
+            resultadoDelta.Text = delta.ToString();
+        }
+
+        private void x1(double a, double b, double c)
+        {
+            double delta = (b * b) - (4 * (a * c));
+            double x1 = (-b + (Math.Sqrt(delta))) / (2 * a);
+
+            if (delta > 0)
+            {
+                resultadoX1.Text = x1.ToString();
+            }
+            else if (delta == 0)
+            {
+                resultadoX1.Text = x1.ToString();
+            }
+            else
+            {
+                resultadoX1.Text = "Equação não possui valor para ser exibido.";
+            }
+        }
+        private void x2(double a, double b, double c)
+        {
+            double delta = (b * b) - (4 * (a * c));
+            double x2 = (-b - (Math.Sqrt(delta))) / (2 * a);
+
+            if (delta > 0)
+            {
+                resultadoX2.Text = x2.ToString();
+            }
+            else if (delta == 0)
+            {
+                resultadoX2.Text = x2.ToString();
+            }
+            else
+            {
+                resultadoX2.Text = "Equação não possui valor para ser exibido.";
+            }
+        }
+
+    }
+}
